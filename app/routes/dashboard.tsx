61ae5751b607ed966e8c8a78ce2803ef84bd572b
Derrick Bol <derrickbol33@gmail.com>
2024-09-17 20:43:26 +0800

    feat: dashboard

---
+++ app/routes/dashboard.tsx
@@ -0,0 +1,27 @@
+import { type LoaderFunctionArgs, type MetaFunction } from '@vercel/remix';
+import { MainLayout } from '~/ui/layouts/main';
+import { IntroductionCarousel } from '~/ui/molecules/introduction-carousel';
+
+export const meta: MetaFunction = () => {
+  return [
+    {
+      title: 'TravelWise | TravelWise with AI',
+    },
+    {
+      name: 'description',
+      content: 'Your AI trip planner',
+    },
+  ];
+};
+
+export const loader = async (args: LoaderFunctionArgs) => {
+  return null;
+};
+
+export default function Index() {
+  return (
+    <MainLayout>
+      <span className="mx-auto my-auto">Coming Soon...</span>
+    </MainLayout>
+  );
+}
