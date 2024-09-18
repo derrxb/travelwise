3df1096d691b637980da087abe62d72f7201904f
Derrick Bol <derrickbol33@gmail.com>
2024-09-19 01:31:46 +0800

    feat: create UserProfile

--- app/domain/travelwise/repositories/user-repository.ts
+++ app/domain/travelwise/repositories/user-repository.ts
@@ -69,6 +69,9 @@ export class UserRepository {
         username: name,
         email,
         password: hashedPassword,
+        UserProfile: {
+          create: {},
+        },
       },
     });
 
